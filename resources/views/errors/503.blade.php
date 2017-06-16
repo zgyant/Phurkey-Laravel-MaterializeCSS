de6bfa0ef0ef9d8e20432f82c6ca7f44f99d01b0
LAZARUS\Jayant <z9yant@gmail.com>
2017-06-16 22:58:42 +0545

    404 and 503 page ;)
    $ php artisan up
    
    $ php artisan down

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+
+Phurkey by Yakana IT Solutions
+Contact: yakanait@gmail.com/css
+Kathmandu, Nepal
+
+-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1" />
+    <title>Scheduled Maintenance| Phurkey by Yakana IT Solutions</title>
+    <link href="http://www.phurkey.com/tools/style.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="https://file.myfontastic.com/n6vo44Re5QaWo8oCKShBs7/icons.css" rel="stylesheet">
+    <script type="text/javascript" src="tools/jquery.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:100,700,200|Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+    <meta name="keywords"
+          content="Second hand, Book, Online, Nepal, Yakana, Store, Buy Books, Sell Books, Exchange Books, Secondhand, phurkey ">
+
+    <meta name="description"
+          content="Phurkey Nepal's largest online book store where you can exchange, buy, sell used or unused books brought to you by Yakana IT Solutions. Nepal's first online second hand book portal">
+
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Coming Soon | Phurkey by Yakana IT Solutions" />
+    <meta property="og:type" content="Phurkey Nepal's largest online book store where you can exchange, buy, sell used or unused books brought to you by Yakana IT Solutions. Nepal's first online second hand book portal" />
+    <meta property="og:url" content="http://www.phurkey.com/" />
+    <meta property="og:image" content="http://phurkey.com/images/cover.PNG" />
+    <meta property="og:description" content="Phurkey Nepal's largest online book store where you can exchange, buy, sell used or unused books brought to you by Yakana IT Solutions. Nepal's first online second hand book portal" />
+    <meta property="og:site_name" content="Phurkey | Phurkey.com" />
+
+</head>
+<body>
+<div class="wrapper">
+    <div class="content-wrap">
+        <img src="images/yakanalogo.PNG"width="120" />
+
+        <div class="header">
+
+            <h1 id="logo">Planned<span style="color: darkred">Break</span></h1>
+        </div>
+
+        <div class="content">
+            <p>We will be back with something new.</p>
+        </div>
+
+
+        <div class="social">
+            <ul>
+                <li><a href="https://facebook.com/phurkey" class="socicon-facebook"></a></li>
+                <li><a href="#" class="socicon-twitter"></a></li>
+                <li><a href="#" class="socicon-instagram"></a></li>
+                <li><a href="#" class="socicon-linkedin"></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!--Start of Tawk.to Script-->
+<script>
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='tawk.js';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+<div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;'><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&utm_campaign=000_logo&utm_medium=website_phurkey&utm_content=footer_img"><img src="https://cloud.githubusercontent.com/assets/23024110/20663010/9968df22-b55e-11e6-941d-edbc894c2b78.png"  alt="www.000webhost.com"></a></div></body>
+</html>
